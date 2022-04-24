f3fff2fe7012f1d63cfe72a9b3f8a512534a503a
Tomáš Oross <tomi.oross@gmail.com>
2022-04-24 11:17:09 +0200

    Pridane prekladanie textov v RegisterController.java

--- src/main/java/vava/project/vavaprojekt/controllers/RegisterController.java
+++ src/main/java/vava/project/vavaprojekt/controllers/RegisterController.java
@@ -2,32 +2,40 @@ package vava.project.vavaprojekt.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import vava.project.vavaprojekt.App;
+import vava.project.vavaprojekt.Language;
 
 import java.util.Locale;
 
 public final class RegisterController extends Controller {
 
+    @FXML Text text_email;
+    @FXML TextField textField_email;
+    @FXML Text text_login;
+    @FXML TextField textField_login;
+    @FXML Text text_password;
+    @FXML PasswordField passwordField_password;
+    @FXML Button button_register;
+
     public RegisterController(App app) {
         super(app);
-
     }
 
     @FXML
     protected void initialize() {
         this.updateLanguage();
-
     }
 
     @Override
     protected void setLanguage(Locale language) {
-
+        text_email.setText(Language.getWord(language, "email"));
+        text_login.setText(Language.getWord(language, "login"));
+        text_password.setText(Language.getWord(language, "password"));
+        button_register.setText(Language.getWord(language, "sign_up"));
     }
-
-    private void exampleButtonAction(ActionEvent event) {
-        // TODO do something here
-    }
-
-
 
 }
