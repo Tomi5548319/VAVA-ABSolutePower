ccbdce102bee46184fdf7f0174ec44eb44e17fc0
Tomáš Oross <tomi.oross@gmail.com>
2022-04-26 11:32:58 +0200

    Uprava own_profile aby sa nacitaval aj description

--- src/main/java/vava/project/vavaprojekt/controllers/OwnProfileController.java
+++ src/main/java/vava/project/vavaprojekt/controllers/OwnProfileController.java
@@ -3,6 +3,7 @@ package vava.project.vavaprojekt.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
@@ -13,7 +14,7 @@ import vava.project.vavaprojekt.data.Sportsman;
 
 public final class OwnProfileController  extends Controller {
     @FXML private Text text_nickname_heading;
-    @FXML private TextFlow textFlow_description;
+    @FXML private TextArea textArea_description;
     @FXML private Text text_nickname_change;
     @FXML private Text text_login;
     @FXML private Text text_act_password;
@@ -34,7 +35,7 @@ public final class OwnProfileController  extends Controller {
     @Override
     protected void initialize() {
         text_nickname_heading.setText(((Sportsman)app.getUser()).getNickname());
-        //textFlow_description.setText(); TODO textFlow nema .setText()...
+        textArea_description.setText(((Sportsman)app.getUser()).getDescription());
         textField_nickname.setText(((Sportsman)app.getUser()).getNickname());
         textField_login.setText((app.getUser()).getLogin());
 
@@ -44,7 +45,7 @@ public final class OwnProfileController  extends Controller {
     private void saveChanges(MouseEvent mouseEvent) {
         // TODO update v sportovcovi (ktory da update na databazu)
         // TODO pri update hesla checkovat ci je stare heslo spravne a ci su obe nove hesla rovnake
-        //String new_desc = textFlow_description.getText(); TODO textFlow nema .getText()...
+        String new_desc = textArea_description.getText();
         String new_nick = textField_nickname.getText();
         String new_login = textField_login.getText();
 
@@ -53,10 +54,13 @@ public final class OwnProfileController  extends Controller {
         String new_pass_check = Password.getHash(passField_confirm_password.getText());
 
         // TODO popup pre udaje ktore sa nepodari aktualizovat
-        //((Sportsman)app.getUser()).updateDescription(new_desc);
+        ((Sportsman)app.getUser()).updateDescription(new_desc);
         ((Sportsman)app.getUser()).updateNickname(new_nick);
         app.getUser().updateLogin(new_login);
-        app.getUser().updatePassword(act_pass, new_pass, new_pass_check);
+        if (app.getUser().updatePassword(act_pass, new_pass, new_pass_check))
+            System.out.println("Heslo bolo zmenene uspesne");
+        else
+            System.out.println("Heslo sa nepodarilo zmenit");
 
     }
 }
